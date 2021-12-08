a4ca6053e8d794d468e946edd5851ad84a9fe86a
KJSashank <ee19b137@smail.iitm.ac.in>
2021-12-08 16:06:36 +0530

    control_mm modified sash

--- FMM/control_mm.v
+++ FMM/control_mm.v
@@ -8,11 +8,13 @@ module control_mm(
 	input [31:0]c1_addr,
 	input [31:0]dim,
 	input we,
+	output we_rf,
 	output [31:0]dmem_addr
 );
 
 reg [31:0] addr_bnk [0:6];
-reg [31:0] a_addr,b_addr,c_addr;
+reg [31:0] dmem_addr;
+reg we_rf;
 
 // initialising registers to 0
     integer i;
@@ -34,21 +36,38 @@ reg [31:0] a_addr,b_addr,c_addr;
 			end
 		end
 	end
-
+	// A= mxn dim[0:10]xdim[11:20], B=nxo dim[11:20]xdim[21:31]
     always @(inst) begin
 		case(inst)
 			1: begin
 				dmem_addr=addr_bnk[0]+addr_bnk[4]; //load next a
-				addr_bnk[4]=addr_bnk[4]+32;
+				addr_bnk[4]=addr_bnk[4]+32; // By default address increments to next 8 words
+				if(addr_bnk[5]<4*addr_bnk[3][21:31])begin	// If address of b_counter< 4*o  & >0 --> then reset a_counter to beginning of row
+					if(addr_bnk[5]!=0)begin
+						addr_bnk[4]=addr_bnk[4]-4*addr_bnk[3][11:20];
+					end
+				end
+				we_rf=1;
 				end
 			2:begin
 				dmem_addr=addr_bnk[1]+addr_bnk[5]; //load next b
-				if (addr_bnk[5]-addr_bnk[1]>= dim[10:20]
-				addr_bnk[5]=addr_bnk[5]+32;   	   //IMP-Change
+				addr_bnk[5]=addr_bnk[5]+4*addr_bnk[3][21:31];   	 //IMP-Change
+				if(addr_bnk[5]>=4*addr_bnk[3][11:20]*addr_bnk[3][21:31])begin
+					if(addr_bnk[5]==4*(addr_bnk[3][11:20]+1)*addr_bnk[3][21:31]-32)begin
+						addr_bnk[5]=0;
+					end else begin
+						addr_bnk[5]=addr_bnk[5]-4*addr_bnk[3][11:20]*addr_bnk[3][21:31]+32;
+					end	
+				end
+				we_rf=1;
 				end	
 			3:begin
 				dmem_addr=addr_bnk[2]+addr_bnk[6]; //store next c
 				addr_bnk[6]=addr_bnk[6]+32;
+				if(addr_bnk[6]==4*addr_bnk[3][0:10]*addr_bnk[3][21:31])begin
+					mm_complete=1;
+				end
+				we_rf=0;
 				end
 
 			4:  
